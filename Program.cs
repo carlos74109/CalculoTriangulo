7d37bec23630d513bf41bd70a9948df9b3b4734b
Carlos <carlos-84@live.com>
2022-01-25 14:42:24 -0300

    ____

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using Exercicio1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo retang = new Retangulo(4, 9);
+
+            
+
+            Console.WriteLine("A área do triangulo é " + retang.area());
+            Console.WriteLine("O perimeto do triangulo é " + retang.perimetro());
+            Console.WriteLine("A diagonal do triangulo é "  +retang.diagonal());
+        }
+    }
+}
---
+++ Retangulo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Exercicio1.Models
+{
+    internal class Retangulo
+    {
+        public double largura;
+        public double altura;
+
+        public Retangulo(double largura, double altura)
+        {
+            this.largura = largura;
+            this.altura = altura;
+        }
+
+        public double area()
+        {
+            return largura * altura;
+        }
+
+        public double perimetro()
+        {
+            return 2 * (largura + altura);
+        }
+
+        public double diagonal()
+        {
+            double diagonal = (largura * largura) + (altura * altura);
+            return Math.Sqrt(diagonal) ;
+        }
+    }
+}
